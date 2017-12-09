dce1aa21bc2c890b444a4ec9b10819828d7c94c1
f0rtung <zadov.vasya@gmail.com>
2017-12-09 22:25:52 +0300

    fix migration

--- install_env.sh
+++ install_env.sh
@@ -3,6 +3,6 @@ python3 -m venv myvenv
 source myvenv/bin/activate
 pip install -r requirements.txt
 cd contacts
-python manage.py makemigrations
+python manage.py makemigrations contacts_app
 python manage.py migrate
 python manage.py runserver
